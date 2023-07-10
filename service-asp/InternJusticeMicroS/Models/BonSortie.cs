ed6fe7482532d6d853e74eb0e0b264047bd65ac2
Moncef <moncefelhilali@gmail.com>
2023-07-10 12:48:03 +0100

    BonSortie Class & Controller

---
+++ service-asp/InternJusticeMicroS/Models/BonSortie.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InternJusticeMicroS.Models
+{
+    [Table("BonEntree", Schema = "dbo")]
+    public class BonSortie : Entity
+    {
+        [Column("idDocument")]
+        public int? idDocument { get; set; }
+
+        [Column("idDemande")]
+        public int idDemande { get; set; }
+
+        [Column("idCreePar")]
+        public int idCreePar { get; set; }
+
+        [Column("dateSortie")]
+        public DateTime dateSortie { get; set; }
+
+        [Column("dateCreation")]
+        public DateTime dateCreation { get; set; }
+    }
+}
