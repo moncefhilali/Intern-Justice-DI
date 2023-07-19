030a7c9e4e5645d61019fb5260631d71f97cddb2
Moncef <moncefelhilali@gmail.com>
2023-07-19 16:01:22 +0100

    Radio Selector

--- client-react/src/components/ProduitAffiche/ProduitAffiche.jsx
+++ client-react/src/components/ProduitAffiche/ProduitAffiche.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
+import "./ProduitAffiche.css";
 
 const ProduitAffiche = () => {
+  useEffect(() => {}, []);
+
   return (
     <div>
       <div className="header">
         <h1>LES PRODUITS</h1>
+      </div>
+      <div className="table-selector">
+        <input type="radio" name="selector" id="radio-EnStock" defaultChecked />
+        <label htmlFor="radio-EnStock">En Stock ()</label>
+        <input type="radio" name="selector" id="radio-Epuise" />
+        <label htmlFor="radio-Epuise">Épuisé ()</label>
+        <input type="radio" name="selector" id="radio-EnRupture" />
+        <label htmlFor="radio-EnRupture">En Rupture ()</label>
+        <label id="label-line"></label>
       </div>
     </div>
   );
