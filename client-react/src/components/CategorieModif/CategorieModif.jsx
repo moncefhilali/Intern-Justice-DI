c55218a14c2dd276d02914293d6b896ef4a39716
Moncef <moncefelhilali@gmail.com>
2023-07-22 17:25:00 +0100

    Categories Display Table

--- client-react/src/components/CategorieModif/CategorieModif.jsx
+++ client-react/src/components/CategorieModif/CategorieModif.jsx
@@ -6,6 +6,29 @@ const CategorieModif = () => {
       <div class="header">
         <h1>LES CATÉGORIES</h1>
       </div>
+      <div className="container">
+        <table className="tab_le">
+          <thead>
+            <tr className="tittre">
+              <th colSpan="4">Nombre de resultats : 9999</th>
+            </tr>
+            <tr className="sousTitre">
+              <th id="th-Center">#</th>
+              <th id="th-Center">Catégorie</th>
+              <th id="th-Center">Catégorie Parent</th>
+              <th id="th-Center">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="tr-ListProduit">
+              <td id="th-Center">1</td>
+              <td>1</td>
+              <td>1</td>
+              <td id="th-Center">Modifier</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
