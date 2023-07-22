cec083948da96b6c8b599a762ed3bba27e5fcf58
Moncef <moncefelhilali@gmail.com>
2023-07-22 17:10:53 +0100

    Categorie Controller HttpGet All

--- service-asp/InternJusticeMicroS/Controllers/CategorieController.cs
+++ service-asp/InternJusticeMicroS/Controllers/CategorieController.cs
@@ -36,6 +36,29 @@ namespace InternJusticeMicroS.Controllers
         }
 
 
+        // select c1.id, c1.Nom, c2.Nom
+        // from Categorie c1 join Categorie c2
+        // on c1.id = c2.idCategorieMere
+        [HttpGet("all")]
+        public async Task<ActionResult<IEnumerable<object>>> GetAllCategories()
+        {
+            if (_internJusticeContext.Categories == null)
+            {
+                return NotFound();
+            }
+            var result = await _internJusticeContext.Categories
+                .Join(_internJusticeContext.Categories, c1 => c1.id, c2 => c2.idCategorieMere, (c1, c2) => new { Categorie1 = c1, Categorie2 = c2 })
+                .Select(x => new
+                {
+                    x.Categorie1.id,
+                    Categorie1Nom = x.Categorie1.Nom,
+                    Categorie2Nom = x.Categorie2.Nom
+                })
+                .ToListAsync();
+            return result;
+        }
+
+
 
     }
 }
