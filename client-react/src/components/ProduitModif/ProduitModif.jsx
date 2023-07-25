375e22d5a02f1080ac99c0bfb3be1db65c5ebf8c
Moncef <moncefelhilali@gmail.com>
2023-07-25 15:42:06 +0100

    Get & Map All Produits

--- client-react/src/components/ProduitModif/ProduitModif.jsx
+++ client-react/src/components/ProduitModif/ProduitModif.jsx
@@ -1,6 +1,27 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const ProduitModif = () => {
+  const [dataProduits, setDataProduits] = useState([]);
+
+  useEffect(() => {
+    getProduits();
+  }, []);
+
+  const getProduits = () => {
+    axios
+      .get("https://localhost:7165/api/Produit/all")
+      .then((result) => {
+        setDataProduits(result.data);
+      })
+      .catch((error) => {
+        toast("Error Produits");
+        console.log(error);
+      });
+  };
+
   return (
     <div>
       <div className="header">
@@ -10,7 +31,7 @@ const ProduitModif = () => {
         <table className="tab_le">
           <thead>
             <tr className="tittre">
-              <th colSpan="7">Nombre de resultats : 9999</th>
+              <th colSpan="7">Nombre de resultats : {dataProduits.length}</th>
             </tr>
             <tr className="sousTitre">
               <th id="th-Center">#</th>
@@ -23,15 +44,21 @@ const ProduitModif = () => {
             </tr>
           </thead>
           <tbody>
-            <tr className="tr-ListProduit">
-              <td>1</td>
-              <td>1</td>
-              <td>1</td>
-              <td>1</td>
-              <td>1</td>
-              <td>1</td>
-              <td>Modifier</td>
-            </tr>
+            {dataProduits.map((opts, i) => (
+              <tr className="tr-ListProduit">
+                <td id="th-Center">{i + 1}</td>
+                <td id="th-Center">{opts.categorie2Nom}</td>
+                <td id="th-Center">{opts.categorie1Nom}</td>
+                <td id="th-Center">{opts.designation}</td>
+                <td id="th-Center">{opts.marque}</td>
+                <td id="th-Center">{opts.qte}</td>
+                <td>
+                  <button id="btn-blue" className="button-ajouter">
+                    Modifier
+                  </button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
